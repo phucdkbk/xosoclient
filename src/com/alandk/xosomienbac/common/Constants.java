822e3ac82d1e1f8454f444eb144e9c3d3124a055
phucdk <phucdk@moon.(none)>
2014-08-15 22:23:00 +0700

    fix ads

--- src/com/alandk/xosomienbac/common/Constants.java
+++ src/com/alandk/xosomienbac/common/Constants.java
@@ -5,9 +5,10 @@ public interface Constants {
 	 * The number of pages (wizard steps) to show in this demo.
 	 */
 	public static final int NUM_PAGES = 5000;
-	
-	public static final int REFRESH_INTERVAL = 10000; // 10s
-	
-	public static final String AD_UNIT_ID = "ca-app-pub-1954954439370083/7603841453";
 
+	public static final int REFRESH_INTERVAL = 10000; // 10s
+
+	// public static final String AD_UNIT_ID =
+	// "ca-app-pub-1954954439370083/7603841453";
+	public static final String AD_UNIT_ID = "ca-app-pub-4664764099542398/2916631060";
 }
